4681995d5190c3c90d587e957632320e128b3197
SanJammin <freddie.farr893@gmail.com>
2024-08-29 21:19:55 +0100

    Create function getComputerChoice

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+function getComputerChoice () {
+    let x = Math.floor(Math.random() * 100) + 1;
+
+    if (x < 33) {
+        return "Rock";
+    } else if (x >= 33 && x < 66) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+}
+
